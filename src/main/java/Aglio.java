3c5fe3ce80e7f21aae5d64e0b99fa2e21743e781
CS Seow <chengxi03@gmail.com>
2026-08-20 16:55:09 +0800

    update chatbot to greet and exit

--- src/main/java/Aglio.java
+++ src/main/java/Aglio.java
@@ -1,5 +1,17 @@
 public class Aglio {
     public static void main(String[] args) {
-        System.out.println("Greetings,I am Aglio!");
+        String banner = "    _         _ _       \n"
+                + "   / \\   __ _| (_) ___  \n"
+                + "  / _ \\ / _` | | |/ _ \\ \n"
+                + " / ___ \\ (_| | | | (_) |\n"
+                + "/_/   \\_\\__, |_|_|\\___/ \n"
+                + "        |___/           \n";
+        System.out.println("____________________________________________________________");
+        System.out.println(banner);
+        System.out.println("Hello, I am Aglio.");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
     }
 }
