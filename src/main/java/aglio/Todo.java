4a8b515a21516b375e830d19ab534389ab1ce46e
CS Seow <chengxi03@gmail.com>
2026-09-02 10:45:21 +0800

    made todo class with inheritance

---
+++ src/main/java/aglio/Todo.java
@@ -0,0 +1,17 @@
+package aglio;
+
+public class Todo extends Task {
+
+    public Todo(String description) {
+        super(description);
+    }
+
+    @Override
+    public String toString() {
+        return "[T]" + super.toString();
+    }
+}
+
+
+
+
